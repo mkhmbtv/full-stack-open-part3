73c7e6778f32da2c779c828065173446c04a492c
your name here <mmakhambetov@yandex.ru>
2021-06-18 02:04:19 +0400

    Complete 3.18

--- index.js
+++ index.js
@@ -32,11 +32,13 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const info = `Phonebook has info for ${persons.length} people`
-    response.send(
-        `<p>${info}</p>
-        ${new Date()}`
-    )
+    Person.find({})
+        .then(persons => {
+            response.send(
+                `<p>Phonebook has info for ${persons.length} people</p>
+                 <p>${new Date()}</p>`
+            )
+        })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -58,15 +60,16 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }   
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {

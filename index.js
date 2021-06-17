44093069eec69f2415465504bc00825fd966d7ae
your name here <mmakhambetov@yandex.ru>
2021-06-18 01:55:31 +0400

    Complete 3.17

--- index.js
+++ index.js
@@ -77,6 +77,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const number = { 
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, number, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT 
